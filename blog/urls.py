a95162038bc3bd07f5ba290b0186a92038cf83b6
Yurii Uhlanov <yuralightman@gmail.com>
2017-04-17 07:46:53 +0300

    Widen functionality, improve security

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
     url(r'^post/(?P<pk>\d+)/?$', views.post_details, name='post_details'),
+    url(r'^post/new/?$', views.add_new_post, name='add_new_post'),
+    url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
+from django.shortcuts import redirect
 from .models import Post
+from .forms import PostEditForm
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -13,3 +15,33 @@ def post_details(request, pk):
     return render(request, 'blog/post_details.html', {
         'post' : post
     })
+
+def add_new_post(request):
+    if request.method == 'POST':
+        form = PostEditForm(request.POST)
+        if form.is_valid():
+            post = save_post_from_form(form, request.user)
+            return redirect('post_details', pk=post.pk)
+    else:
+        form = PostEditForm()
+        return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostEditForm(request.POST, instance=post)
+        if form.is_valid():
+            post = save_post_from_form(form, request.user)
+            return redirect('post_details', pk=post.pk)
+    else:
+        form = PostEditForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def save_post_from_form(form, user):
+    post = form.save(commit=False)
+    post.author = user
+    post.published_date = timezone.now()
+    post.save()
+    return post
+
+
