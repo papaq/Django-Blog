2083177cd210a5dcfb98b4960af607825c41e36c
Yurii Uhlanov <yuralightman@gmail.com>
2017-04-21 20:53:49 +0300

    Add extra functionality

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     url(r'^post/(?P<pk>\d+)/?$', views.post_details, name='post_details'),
     url(r'^post/new/?$', views.add_new_post, name='add_new_post'),
     url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
+    url(r'^drafts/$', views.post_draft_list, name='post_draft_list'),
+    url(r'^post/(?P<pk>\d+)/publish/$', views.post_publish, name='post_publish'),
+    url(r'^post/(?P<pk>\d+)/delete/$', views.post_delete, name='post_delete'),
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 from django.shortcuts import redirect
 from .models import Post
 from .forms import PostEditForm
+from django.contrib.auth.decorators import login_required
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -16,6 +17,7 @@ def post_details(request, pk):
         'post' : post
     })
 
+@login_required
 def add_new_post(request):
     if request.method == 'POST':
         form = PostEditForm(request.POST)
@@ -26,6 +28,7 @@ def add_new_post(request):
         form = PostEditForm()
         return render(request, 'blog/post_edit.html', {'form': form})
 
+@login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -40,8 +43,24 @@ def post_edit(request, pk):
 def save_post_from_form(form, user):
     post = form.save(commit=False)
     post.author = user
-    post.published_date = timezone.now()
+    #post.published_date = timezone.now()
     post.save()
     return post
 
+@login_required
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+@login_required
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_details', pk=pk)
+
+@login_required
+def post_delete(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
 
